92031647193870875f647b7197c878b95824530e
Rodrigo Coelho <rodrigo.coelho@hotmail.com.br>
2026-03-05 15:41:33 -0400

    fix: resolve merge conflict in App.vue and restore hello world header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
-<template>
-  <div>
-    <h1>hello world</h1>
-=======
 <script setup lang="ts"></script>
 
 <template>
   <div>
-    <span>Hello World</span>
->>>>>>> 95087b60d3cdfe54a96f814cc0f08b35f9d3d689
+    <h1>hello world</h1>
   </div>
 </template>
 
